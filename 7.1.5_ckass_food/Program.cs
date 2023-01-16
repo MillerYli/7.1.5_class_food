c6fdc519b1d665fecb6e4eb45de0897868a0ffbe
Yulia Miller <miller82.98@mail.ru>
2023-01-16 10:29:51 -0800

    Добавьте файлы проекта.

---
+++ 7.1.5_ckass_food/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Food
+{
+
+}
+class Fruit: Food
+{
+
+}
+
+class Vegetables: Food
+{
+
+}
+class Apple: Fruit
+{
+
+}
+class Banana: Fruit
+{
+
+}
+class Pear: Fruit
+{
+
+}
+class Potato: Vegetables
+{
+
+}
+class Carrot: Vegetables
+{
+
+}
